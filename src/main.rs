ae8b7531143c90d81aa2e21dcd757a5e32e69665
Redempt <instantiable@gmail.com>
2021-12-19 01:26:16 -0500

    Improve repl to show errors instead of panicking

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use std::io::*;
 fn main() {
     std::io::stdin().lock().lines().for_each(|l| {
         let line = l.unwrap();
-        let parsed: JSONValue = line.parse().unwrap();
-        println!("{:?}", parsed);
+        let parsed = line.parse::<JSONValue>();
+        match parsed {
+            Ok(v) => println!("{:?}", v),
+            Err(e) => println!("{:?}", e)
+        }
     });
 }
